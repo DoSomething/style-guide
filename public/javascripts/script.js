39fb15f076594bf50fcb3714c49948ab6233601f
mmwtsn <mwatson@dosomething.org>
2014-01-14 15:41:30 -0500

    Renames JavaScript directory

---
+++ public/javascripts/script.js
@@ -0,0 +1,16 @@
+// Base JavaScript file
+$(function() {
+  // Call function to pin sticky nav
+  NEUE.pinToTop();
+  
+  // Call ZeroClipboard for Flash-based copying to the user's clipboard
+  var clipboard = new ZeroClipboard( $('.clipboard') );
+
+  clipboard.on("load", function(client) {
+    client.on( "complete", function(client, args) {
+      // `this` is the element that was clicked
+      this.style.display = "none";
+      alert("Copied text to clipboard: " + args.text );
+    });
+  });
+})
